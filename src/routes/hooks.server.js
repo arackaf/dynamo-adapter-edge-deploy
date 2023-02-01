cfd6eed47da4aabc7185ba0ce1f998b49486cd62
Adam Rackis <adamrackis@hotmail.com>
2023-02-01 17:51:27 -0600

    add default handler

--- src/routes/hooks.server.js
+++ src/routes/hooks.server.js
@@ -1,3 +1,12 @@
 import { DynamoDBAdapter } from '@next-auth/dynamodb-adapter';
 
 console.log(typeof DynamoDBAdapter);
+
+export async function handle({ event, resolve }) {
+	if (event.url.pathname.startsWith('/custom')) {
+		return new Response('custom response');
+	}
+
+	const response = await resolve(event);
+	return response;
+}
