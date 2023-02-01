458b372126fc9f2b38eee35604c3e80637c24037
Adam Rackis <adamrackis@hotmail.com>
2023-02-01 17:53:32 -0600

    Force dynamo adapter to run

--- src/routes/hooks.server.js
+++ src/routes/hooks.server.js
@@ -2,6 +2,10 @@ import { DynamoDBAdapter } from '@next-auth/dynamodb-adapter';
 
 console.log(typeof DynamoDBAdapter);
 
+export function simulateDynamoAdapter() {
+	return DynamoDBAdapter(null, { tableName: 'DYNAMO_AUTH_TABLE' });
+}
+
 export async function handle({ event, resolve }) {
 	if (event.url.pathname.startsWith('/custom')) {
 		return new Response('custom response');
