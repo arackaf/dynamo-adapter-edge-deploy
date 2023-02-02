41e664d92ed0c04b957cd73ac6bd2e2b42412696
Adam Rackis <adamrackis@hotmail.com>
2023-02-01 18:00:43 -0600

    still trying to repro

--- src/routes/hooks.server.js
+++ src/routes/hooks.server.js
@@ -3,7 +3,9 @@ import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb';
 
 import { SvelteKitAuth } from '@auth/sveltekit';
 
-export function simulateDynamoAdapter() {
+var auth = {};
+
+try {
 	const dynamoConfig = {};
 	const client = DynamoDBDocument.from(new DynamoDB(dynamoConfig), {
 		marshallOptions: {
@@ -13,14 +15,17 @@ export function simulateDynamoAdapter() {
 		}
 	});
 
-	return SvelteKitAuth({
+	auth = SvelteKitAuth({
 		adapter: DynamoDBAdapter(client, { tableName: 'DYNAMO_AUTH_TABLE' })
 	});
-}
+} catch (er) {}
 
 export async function handle({ event, resolve }) {
 	if (event.url.pathname.startsWith('/custom')) {
 		return new Response('custom response');
+	}
+
+	if (auth?.()) {
 	}
 
 	const response = await resolve(event);
